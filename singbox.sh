9ca119237ef1f885beb775bb67b9a48d940eea1b
2777nn <129933210+2777nn@users.noreply.github.com>
2026-08-07 15:39:54 +0800

    Update singbox.sh

--- singbox.sh
+++ singbox.sh
@@ -5314,7 +5314,7 @@ _xray_features() {
     
     if [ -f "$script_path" ]; then
         chmod +x "$script_path"
-        source "$script_path"
+        bash "$script_path"
     else
         _error "找不到 Xray 管理脚本: ${script_path}"
     fi
